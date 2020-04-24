ca5793676ae2f3e929f0ac188a4d5a27ab1ee8fe
HIRONORI KIKUCHI <hironori.kikuchi.pgm@gmail.com>
2020-04-24 21:19:25 +0700

    add signup view

---
+++ src/components/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" placeholder="email" v-model="Email" />
+    <input type="text" placeholder="password" v-model="Password" />
+    <button @click="signup">Signup</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Auth from '@/database/firebase-auth'
+export default Vue.extend({
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signup: function () {
+      Auth.signup(this.email, this.password)
+        .then(cred => {
+          if (cred) {
+            this.$router.push({ name: 'Home' })
+          } else {
+            this.$router.push({})
+          }
+        })
+    }
+  }
+})
+</script>
